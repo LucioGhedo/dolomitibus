2d82757f999c40ed28d8503e3bbc4f67348c9ba9
LucioGhedo <lucioghedina@gmail.com>
2022-06-30 18:21:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+      giornomeno: dayjs().subtract(1.08, 'hours').format('MM/DD/YYYY HH:mm:ss'),
+      data: dayjs().format('HH:mm:ss'),
+      giorno: dayjs().subtract(1, 'hours').format('MM/DD/YYYY HH:mm:ss')
+    },
+    methods: {
+      updateTime(data) {
+        this.data = dayjs().format('HH:mm:ss'),
+        setInterval(this.updateTime, 1000)
+      }
+    },
+  })

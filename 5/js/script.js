b0617edbe325460515210d7d15bc1c5f04ec2666
LucioGhedo <lucioghedina@gmail.com>
2022-07-16 15:38:36 +0200

    updaate

--- 5/js/script.js
+++ 5/js/script.js
@@ -3,7 +3,7 @@ var app = new Vue({
     data: {
       giornomeno: dayjs().subtract(1.08, 'hours').format('MM/DD/YYYY HH:mm:ss'),
       data: dayjs().format('HH:mm:ss'),
-      giorno: dayjs().subtract(5, 'minutes').format('MM/DD/YYYY HH:mm:ss')
+      giorno: dayjs().subtract(8, 'minutes').format('MM/DD/YYYY HH:mm:ss')
     },
     methods: {
       updateTime(data) {
